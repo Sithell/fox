c84b6f4a43130958ab7f05eee350cf8ba4d75fe2
Sithell <thesithell@gmail.com>
2023-03-04 13:24:08 +0300

    refactor(cmd): pass aefro.fs as dependency in all commands

--- cmd/install.go
+++ cmd/install.go
@@ -13,64 +13,63 @@ import (
 	"strings"
 )
 
-// installCmd represents the "install" command
-var installCmd = &cobra.Command{
-	Use:   "install",
-	Short: "Install mod by a git link",
-	Long:  `Install mod by a git link.`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) < 1 {
-			return errors.New("missing argument: url")
-		}
-		var url = args[0]
-		if _, err := u.ParseRequestURI(url); err != nil {
-			return fmt.Errorf("not a valid url: %s", url)
-		}
-		if url[len(url)-4:] != ".git" {
-			return fmt.Errorf("not a git repository: %s", url)
-		}
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		var url = args[0]
-
-		fs := afero.NewOsFs()
-
-		chromePath, err := internal.PrepareFirefox(fs)
-		if err != nil {
-			panic(err)
-		}
-
-		re, err := regexp.Compile("([A-Za-z0-9_\\-]+).git$")
-		if err != nil {
-			panic(err)
-		}
-		repoName := re.FindAllStringSubmatch(url, -1)[0][1]
-		fmt.Printf("Installing %s to %s\n", repoName, chromePath)
-
-		_, err = git.PlainClone(chromePath+"/"+repoName, false, &git.CloneOptions{
-			URL:      url,
-			Progress: os.Stdout,
-		})
-		if err != nil {
-			if err.Error() == "repository already exists" {
-				fmt.Println(err)
-			} else {
-				panic(err)
+func NewInstallCmd(fs afero.Fs) *cobra.Command {
+	return &cobra.Command{
+		Use:   "install",
+		Short: "Install mod by a git link",
+		Long:  `Install mod by a git link.`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return errors.New("missing argument: url")
 			}
-		}
+			var url = args[0]
+			if _, err := u.ParseRequestURI(url); err != nil {
+				return fmt.Errorf("not a valid url: %s", url)
+			}
+			if url[len(url)-4:] != ".git" {
+				return fmt.Errorf("not a git repository: %s", url)
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			var url = args[0]
 
-		userFiles := internal.LocateUserFiles(fs, chromePath+"/"+repoName)
-		for userFileName, pathInRepo := range userFiles {
-			filename := chromePath + "/" + string(userFileName)
-			bytes, _ := os.ReadFile(filename)
-			content := addImportToFile(string(bytes), repoName+"/"+pathInRepo)
-			err = os.WriteFile(filename, []byte(content), 0644)
+			chromePath, err := internal.PrepareFirefox(fs)
 			if err != nil {
 				panic(err)
 			}
-		}
-	},
+
+			re, err := regexp.Compile("([A-Za-z0-9_\\-]+).git$")
+			if err != nil {
+				panic(err)
+			}
+			repoName := re.FindAllStringSubmatch(url, -1)[0][1]
+			fmt.Printf("Installing %s to %s\n", repoName, chromePath)
+
+			_, err = git.PlainClone(chromePath+"/"+repoName, false, &git.CloneOptions{
+				URL:      url,
+				Progress: os.Stdout,
+			})
+			if err != nil {
+				if err.Error() == "repository already exists" {
+					fmt.Println(err)
+				} else {
+					panic(err)
+				}
+			}
+
+			userFiles := internal.LocateUserFiles(fs, chromePath+"/"+repoName)
+			for userFileName, pathInRepo := range userFiles {
+				filename := chromePath + "/" + string(userFileName)
+				bytes, _ := afero.ReadFile(fs, filename)
+				content := addImportToFile(string(bytes), repoName+"/"+pathInRepo)
+				err = afero.WriteFile(fs, filename, []byte(content), 0644)
+				if err != nil {
+					panic(err)
+				}
+			}
+		},
+	}
 }
 
 func addImportToFile(s string, path string) string {
@@ -101,15 +100,5 @@ func addImportToRegion(region string, path string) string {
 }
 
 func init() {
-	rootCmd.AddCommand(installCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// installCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// installCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	rootCmd.AddCommand(NewInstallCmd(afero.NewOsFs()))
 }
--- cmd/remove.go
+++ cmd/remove.go
@@ -4,47 +4,48 @@ import (
 	"errors"
 	"fmt"
 	"github.com/Sithell/fox/internal"
+	"github.com/spf13/afero"
 	"github.com/spf13/cobra"
-	"os"
 	"strings"
 )
 
-// removeCmd represents the remove command
-var removeCmd = &cobra.Command{
-	Use:   "remove",
-	Short: "Remove installed mod by name",
-	Long:  `Remove command accepts a single argument: name of the mod folder inside of the chrome directory`,
-	Args: func(cmd *cobra.Command, args []string) error {
-		if len(args) != 1 {
-			return errors.New("Remove command accepts a single argument: name of the mod folder inside of the chrome directory")
-		}
-		return nil
-	},
-	Run: func(cmd *cobra.Command, args []string) {
-		var repoName = args[0]
+func NewRemoveCmd(fs afero.Fs) *cobra.Command {
+	return &cobra.Command{
+		Use:   "remove",
+		Short: "Remove installed mod by name",
+		Long:  `Remove command accepts a single argument: name of the mod folder inside of the chrome directory`,
+		Args: func(cmd *cobra.Command, args []string) error {
+			if len(args) != 1 {
+				return errors.New("Remove command accepts a single argument: name of the mod folder inside of the chrome directory")
+			}
+			return nil
+		},
+		Run: func(cmd *cobra.Command, args []string) {
+			var repoName = args[0]
 
-		chromePath, err := internal.PrepareFirefox()
-		if err != nil {
-			panic(err)
-		}
-
-		err = os.RemoveAll(chromePath + "/" + repoName)
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Deleted %s directory from %s\n", repoName, chromePath)
-
-		for _, userFileName := range []internal.UserFile{internal.Chrome, internal.Content} {
-			filename := chromePath + "/" + string(userFileName)
-			bytes, _ := os.ReadFile(filename)
-			content := removeImportsFromFile(string(bytes), repoName)
-			err = os.WriteFile(filename, []byte(content), 0644)
+			chromePath, err := internal.PrepareFirefox(fs)
 			if err != nil {
 				panic(err)
 			}
-			fmt.Printf("Removed imports from %s\n", userFileName)
-		}
-	},
+
+			err = fs.RemoveAll(chromePath + "/" + repoName)
+			if err != nil {
+				panic(err)
+			}
+			fmt.Printf("Deleted %s directory from %s\n", repoName, chromePath)
+
+			for _, userFileName := range []internal.UserFile{internal.Chrome, internal.Content} {
+				filename := chromePath + "/" + string(userFileName)
+				bytes, _ := afero.ReadFile(fs, filename)
+				content := removeImportsFromFile(string(bytes), repoName)
+				err = afero.WriteFile(fs, filename, []byte(content), 0644)
+				if err != nil {
+					panic(err)
+				}
+				fmt.Printf("Removed imports from %s\n", userFileName)
+			}
+		},
+	}
 }
 
 func removeImportsFromFile(s string, dirName string) string {
@@ -59,5 +60,5 @@ func removeImportsFromFile(s string, dirName string) string {
 }
 
 func init() {
-	rootCmd.AddCommand(removeCmd)
+	rootCmd.AddCommand(NewRemoveCmd(afero.NewOsFs()))
 }
